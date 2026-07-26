25b83290287bd1b61e2f9d3b626ab5fd000a3323
itssourov13 <sourovmondolb@gmail.com>
2026-07-26 09:10:41 +0000

    Update Supabase publishable key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://iausxfvlhqtlcoaovoiv.supabase.co"
+const supabaseUrl = "https://iausxfvlhqtlcoaovoiv.supabase.co";
 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhdXN4ZnZsaHF0bGNvYW92b2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NDIzNjYsImV4cCI6MjEwMDExODM2Nn0.GF9N-dIbyYgScR1JdPtKe35pENRZo42d7G8DLupKxyo"
+const supabaseAnonKey = "sb_publishable_HIBUR248IwLNHCCYR81-CA_8ZdSUAZS";
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
-)
+);
